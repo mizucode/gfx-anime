d2524f24d484ba7e1d16f80190a53684aead1a89
mizucode <mizurisysthensis3@gmail.com>
2023-12-13 12:13:21 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["./public/**/*.{html,js}"],
+	theme: {
+		container: {
+			center: true,
+			padding: "16px",
+		},
+		extend: {
+			fontFamily: {
+				inter: ["Inter"],
+				poppins: ["Poppins"],
+			},
+			colors: {
+				dasar: "#29aae1",
+				"blue-background": "#EDFBFC",
+				ungu: "#29AAE1",
+				"text-abu": "#535F6C",
+				"red-icon": "#F96C6C",
+				oren: "#F0864E",
+				"putih-dasar": "#F5F5FA",
+				dark: "#17171F",
+			},
+		},
+	},
+	plugins: [],
+};
